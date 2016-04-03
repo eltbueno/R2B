a7d377f65ba444c0ae73a9915615b328937becc0
Elton Rodrigues <eltbueno@hotmail.com>
2016-04-02 21:30:41 -0300

    alterando welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Laravel 5 - Estamos on line - Equipe R2B</div>
             </div>
         </div>
     </body>
