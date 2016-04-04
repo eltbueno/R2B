145c31e22dbd010d562f1081aa821a5b0a6d5ae3
Elton Rodrigues <eltbueno@hotmail.com>
2016-04-04 16:26:48 -0300

    alterando view cidade

--- resources/views/configuracao/cidade/cidade.blade.php
+++ resources/views/configuracao/cidade/cidade.blade.php
@@ -1,8 +1,39 @@
-<?php
+@extends('layout.conf')
+@section('menuconf')
+@stop
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+@section('conteudo')
+<section id='cidbase'>
+<script type="text/javascript">
+    function novo()
+    {
+        location.href='/cid_novo';
+    }
+    
+    function para()
+    {
+        endforech;
+    }
 
+</script>
+
+<h2> Cadastro de Cidades</h2>
+<table border='1'>
+    <tr>
+        <td>Nome</td>
+        <td>UF/Estado</td>
+    </tr>
+    <tr>
+    <form method="get" action="/cid_busca">
+        <td><input type="text" name='cid_id'</td>
+        <td><input type="text" name='cid_uf'</td>
+        <td><input type='submit' value='Buscar'/></td>
+        <td><input type="button" value='Nova Cidade'onclick="novo()"></input> </td>
+    </form>
+    </tr>
+    
+    
+</table>
+@yield('cidbase')
+</section>
+@stop
