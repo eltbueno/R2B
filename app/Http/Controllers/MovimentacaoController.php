e5c0e6de4cb98b00f6593edd0bc2d011f6bd4611
Elton Rodrigues <eltbueno@hotmail.com>
2016-04-18 00:22:42 -0300

    continuando movimentacao

--- app/Http/Controllers/MovimentacaoController.php
+++ app/Http/Controllers/MovimentacaoController.php
@@ -35,17 +35,43 @@ class MovimentacaoController extends Controller{
         $placa = Request::input('placa');
         $datainicio = Request::input('data_inicio');
         $datafim = Request::input('data_fim');
-        
+        //return $datafim . '<br>' . $datainicio; 
         if ($datafim == "" && $datainicio == "")
         {
-            $datafim = date('Y-m-d H:i');
-            
-            $datainicio = date('Y-m-d H:i');
-            
-            return $datafim . '<br>' . $datainicio;
-            
-            $mov = \r2b\Movimentacao::wherePlaca($placa)->get();
+            // pc do elton a hora esta adiantada em 3 horas
+            // no servidor da amazon está correta
+            // procurar depois acertar no pc
+            // busca abaixo retorna as movimentações de 3 meses atras da placa especificada
+            $datafim = date('Y-m-d H:i');            
+            $datainicio = date('Y-m-d H:i' ,strtotime($datafim. '- 90 days')) ;            
+            //return $datafim . '<br>' . $datainicio;            
+            $mov = \r2b\Movimentacao::where('placa','=',$placa)
+                    ->where('data_inicio','>=',$datainicio)
+                    ->get();            
+        }elseif($datafim != "" && $datainicio == "")
+        {
+            $datafim = date('Y-m-d H:i' ,strtotime($datafim.'+ 23 hours + 59 minutes')) ;            
+            //return $datafim;
+            $mov = \r2b\Movimentacao::where('placa','=',$placa)
+                    ->where('data_fim','<=',$datafim)
+                    ->get();            
+        }elseif($datafim == "" && $datainicio != "")
+        {
+            $datainicio = date('Y-m-d H:i' ,strtotime($datainicio)) ; 
+            //return $datainicio;
+            $mov = \r2b\Movimentacao::where('placa','=',$placa)
+                    ->where('data_inicio','>=',$datainicio)
+                    ->get();            
+        }else
+        {
+            $datafim = date('Y-m-d H:i' ,strtotime($datafim.'+ 23 hours + 59 minutes')) ;
+            $datainicio = date('Y-m-d H:i' ,strtotime($datainicio)) ;
+            $mov = \r2b\Movimentacao::where('placa','=',$placa)
+                    ->where('data_inicio','>=',$datainicio)
+                    ->where('data_fim','<=',$datafim)
+                    ->get();            
         }
+        
         
         //return $mov;
         return view('movimentacao.movimentacao_detalhe')->with(array('mov'=>$mov,'placa'=>$placa));
