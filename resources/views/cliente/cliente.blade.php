66e0a6a07846b838d82324cb4327cd8a139bf6e7
Elton Rodrigues <eltbueno@hotmail.com>
2016-05-15 11:37:48 -0300

    ajuste no layout

--- resources/views/cliente/cliente.blade.php
+++ resources/views/cliente/cliente.blade.php
@@ -1,16 +1,16 @@
 @extends('principal')
 @section('conteudo')
 <section id='clibase'>
-<h3>Cadastro - Clientes</h3>
+<h2>Cadastro - Clientes</h2>
 <script type="text/javascript">
     function novo()
     {
         location.href='/cliente_novo';
     }
 </script>
-
-
-<table border='0'>
+<tr>
+<form class="form-inline">
+<!-- <table border='0'>
     <tr>
         <td>Codigo</td>
         <td>Nome</td>        
@@ -32,10 +32,40 @@
         </td>
         <td><input type='submit' value='Buscar'/></td>
         <td><input type="button" value='Novo Cliente'onclick="novo()"></input> </td>
-        </form>
+        
+        </table> 
+       
+        
+        </form>-->
+		
+<form class="form-inline">
+  <div class="form-group">
+    <label for="name3">Name</label>
+    <input type="text" class="form-control" id="exampleInputName2" placeholder="Seu Nome">
+  </div>
+  </br>
+  <div class="form-group">
+    <label for="email2">Email</label>
+    <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Seu Email">
+  </div>
+  </br>
+  <form method="get" action="/cliente_busca">
+  <div class="form-group">
+  <label for="fj">Fisico/Juridico</label>
+  <select class="form-control">
+  <option value=''></option>
+  <option value='1'>Fisico</option>
+  <option value='2'>Juridico</option>
+</select>
+</div>
+</form>
+  <button type="submit" class="btn btn-primary">Buscar</button>
+  <button type="button" onclick="novo()" class="btn btn-default">Novo Cliente</button>
+</form>
     </tr>
     
-</table>
+
+
 @yield('clibase')
 </section>
 @stop
